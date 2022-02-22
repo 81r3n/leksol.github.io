5116a13b6ac11cee7f3d7addc1aeb5e4d933ba61
Birendra Chhetri <birendra@abit.bt>
2022-02-22 15:39:04 +0600

    change

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            var element = document.querySelector(".navbar");
-            element.classList.add("shadowed");
-            var logo = document.querySelector(".logo");
-            logo.classList.add("change");
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         if ($(this).scrollTop() > 100) {
+//             var element = document.querySelector(".navbar");
+//             element.classList.add("shadowed");
+//             var logo = document.querySelector(".logo");
+//             logo.classList.add("change");
 
-        } else {
-            var element = document.querySelector(".navbar");
-            element.classList.remove('shadowed');
-            var logo = document.querySelector(".logo");
-            logo.classList.remove("change");
+//         } else {
+//             var element = document.querySelector(".navbar");
+//             element.classList.remove('shadowed');
+//             var logo = document.querySelector(".logo");
+//             logo.classList.remove("change");
 
-        }
-    });
-});
+//         }
+//     });
+// });
 
 
 
