4c10242318e818cfdf3dfb2575d46f5ff4406222
Birendra Chhetri <birendra@abit.bt>
2022-03-14 01:17:14 +0600

    more pages added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
         var element = document.querySelector(".navbar");
         element.classList.add("shadowed");
@@ -36,12 +36,12 @@ let aboutus = gsap.timeline({
     }
 })
 aboutus.to(".secondary-heading.animated span", {
-        opacity: 1,
-        stagger: {
-            amount: 1,
-            from: "end"
-        },
-    })
+    opacity: 1,
+    stagger: {
+        amount: 1,
+        from: "end"
+    },
+})
     .from(".description.about, .my-btn.about", {
         duration: 1,
         opacity: 0,
@@ -52,7 +52,7 @@ aboutus.to(".secondary-heading.animated span", {
 
 // business titles fade stagger
 const businessItems = document.querySelectorAll(".item");
-businessItems.forEach(function(item, index) {
+businessItems.forEach(function (item, index) {
     let business = gsap.timeline({
         scrollTrigger: {
             trigger: item.querySelector(".business-heading"),
@@ -64,10 +64,10 @@ businessItems.forEach(function(item, index) {
         defaults: { ease: "power1.inOut" }
     })
     business.to(item.querySelectorAll('.business-heading span'), {
-            opacity: 1,
-            stagger: { amount: 1 },
-            ease: "power1.out"
-        })
+        opacity: 1,
+        stagger: { amount: 1 },
+        ease: "power1.out"
+    })
         .from(item.querySelector('.description.business'), {
             duration: 1,
             opacity: 0,
@@ -101,7 +101,7 @@ gsap.to(".businessText", {
 
 // business items
 const items = document.querySelectorAll(".item");
-items.forEach(function(item, index) {
+items.forEach(function (item, index) {
     const tlBusiness = gsap.timeline({ defaults: { ease: 'slowMo.out' }, paused: true });
     tlBusiness.to(item.querySelector('.businessImageOverlay'), { duration: 0.35, opacity: 0 })
         .to(item.querySelector('.businessImage'), { duration: 5, scale: 1.1 }, '-=0.35');
@@ -130,24 +130,24 @@ let mvSection = gsap.timeline({
     }
 })
 mvSection.to(".mv-section-heading span", {
-        opacity: 1,
-        stagger: { amount: 1.2 },
-    })
+    opacity: 1,
+    stagger: { amount: 1.2 },
+})
     .from(".mv-card", { duration: 2, y: 35, opacity: 0, stagger: 0.2 }, "-=1.6")
 // .from(".footer-box", { duration: 2, opacity: 0, scale: 0.95, ease: "slowMo.out" }, '-=2');
 // Internal pages
 gsap.from(".page-banner img", { duration: 3, x: -100, y: -100, opacity: 0 });
 let pages = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".page-heading",
-            start: "top 70%",
-            end: "=+200 bottom",
-            toggleActions: "play none none reverse"
-        },
-        defaults: {
-            ease: "power1.inOut"
-        }
-    })
+    scrollTrigger: {
+        trigger: ".page-heading",
+        start: "top 70%",
+        end: "=+200 bottom",
+        toggleActions: "play none none reverse"
+    },
+    defaults: {
+        ease: "power1.inOut"
+    }
+})
     .to(".page-heading span", {
         opacity: 1,
         y: 0,
